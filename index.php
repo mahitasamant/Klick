dd60230fbf2d9a43b9fe8a6db0b30f54fdfbf830
ashi-jo <64354607+ashi-jo@users.noreply.github.com>
2021-05-23 19:07:15 +0530

    added php files

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        #ques{
+            min-height: 433px;
+        }
+    </style>
+    <title>Welcome to Klick</title>
+    
+    <link rel="stylesheet" href="welcome-style.css">
+</head>
+
+</head>
+<body>
+    <?php include '_dbconnect.php';?>
+    <?php include 'header.php';?>
+  
+  <main>
+    <section id="home" class="sectiona">
+  <div class="container">
+    <div>
+    <h1>The Ultimate Doubt solving forum</h1>
+    <p>
+    Klick is an academic forum where young minds can discuss and debate a vast array of academic and conceptual problems. Our idea is to create an open to all doubt-solving forum where people can post questions they are unable to solve or understand. These problems unlike being textbook problems will be problems asked by peers. Other students on the forum can help solve these queries. Asking doubts comes with a sense of shame and fear, we want to this to be a platform where students can put forward their difficulties without feeling judged. We firmly believe that no one can explain concepts to young children as well and as empathetically as peers their age. Healthy academic interaction is a significant part of learning communication skills, teamwork, and conflict resolution which are an important part of education. This is a safe space to ask your doubts and help young minds just like you, Interact with young minds from across the globe and socities and let your imagination and creativity run wild!
+    </p>
+    <a href="category.php
+    " class="btn">EXPOLRE CATEGORIES</a>
+  </div>
+  <img id="header-img" src="1.png" alt=""/>
+  
+  </div>
+</section>
+
+<footer class="footer" style="background:#353535;	color:#d9d9d9;	text-align:center;"><p style="margin:0;	padding:10px"><a target="blank" href="#" style="color:#d9d9d9; ">Klick 2021<a>- © All rights reserved </p></footer>
+	 
+  </main>
+</body>  
+
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+</html>
